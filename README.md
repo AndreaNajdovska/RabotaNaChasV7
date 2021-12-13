d32bdcfeb1efde98ed420d0ee0235bde4f87442a
AndreaNajdovska <andrea.najdovska@gmail.com>
2021-12-13 20:29:15 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# RabotaNaChasV7
---
+++ vtora vezhba/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MetodiZaListaNiza m = new MetodiZaListaNiza();
+        List<String> lista = new ArrayList<String>();
+        lista.add("A");
+        lista.add("B");
+        lista.add("V");
+        m.listToArray(lista);
+
+        String [] niza = {"a","b","v"};
+        m.arrayToList(niza);
+    }
+}
